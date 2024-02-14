d08d9a52add8eb3f7faaaed1cfba004093e27089
Bea Serrano Lucio <78020428+BeaSerrano@users.noreply.github.com>
2024-02-14 17:29:03 +0100

    Update index.js from components

--- components/index.js
+++ components/index.js
@@ -1,6 +1,6 @@
 export * from "./Header/Header";
 export * from "./Footer/Footer";
 export * from "./CardPokemons/CardPokemons";
-export * from "./Spinner/Spinner";
+export * from "./spinner/Spinner";
 export * from "./ButtonFIlter/ButtonFilter";
 export * from "./spinner/SpinnerButton";
